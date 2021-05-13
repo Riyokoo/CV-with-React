785c23038ba0a60ef09f2cf199878b17f3d912dd
Vladi Ciuculescu <vladimir.ciuculescu@titeica.com>
2021-05-13 13:13:07 +0300

    Added the sheet and styled background

---
+++ src/components/CV-Container/CV-Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CV-Container.css';
+
+export default class CvContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className = "cv-container">
+                
+            </div>
+        )
+    }
+}
